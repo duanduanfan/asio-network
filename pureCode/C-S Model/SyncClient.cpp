4a269b2dcba4e4722963da590788dc3261cce601
df <3426345650@qq.com>
2026-04-04 18:55:43 +0800

    学习了通过boost::asio创建一个C/S 服务器

---
+++ pureCode/C-S Model/SyncClient.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include "boost/asio.hpp"
+using namespace boost::asio::ip;
+using namespace std;
+const int MAX_LENGTH = 1024;
+
+int main()
+{
+    try {
+        //创建一个上下文服务
+        boost::asio::io_context ioc;
+        tcp::endpoint remote_ep(address::from_string("127.0.0.1"), 10086);
+        tcp::socket sock(ioc);
+        boost::system::error_code error = boost::asio::error::host_not_found;
+        sock.connect(remote_ep, error);
+        if (error) {
+            cout << "connect failed, code is " << error.value() << " error msg is " 
+                << error.message() << endl;
+            return 0;
+        }
+
+        cout << "Enter message: ";
+        char request[MAX_LENGTH];
+        std::cin.getline(request, MAX_LENGTH);
+        size_t request_length = strlen(request);
+        boost::asio::write(sock, boost::asio::buffer(request, request_length));
+
+        char reply[MAX_LENGTH];
+        size_t reply_length = boost::asio::read(sock,
+            boost::asio::buffer(reply, request_length));
+        cout << "Reply is: ";
+        cout.write(reply, reply_length);
+        cout << "\n";
+    }
+    catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << endl;
+    }
+    return 0;
+}
---
+++ pureCode/C-S Model/SyncServer.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <boost/asio.hpp>
+#include <set>
+#include <memory>
+using boost::asio::ip::tcp;
+const int max_length = 1024;
+typedef std::shared_ptr<tcp::socket> socket_ptr;
+std::set<std::shared_ptr<std::thread>> thread_set;
+//using namespace std;
+void session(socket_ptr sock)
+{
+	try {
+
+		for (;;) {
+			char data[max_length];
+			memset(data, '\0', max_length);
+			boost::system::error_code error;
+			/*size_t length = boost::asio::read(sock,
+				boost::asio::buffer(data, max_length), error);*/
+			size_t length = sock->read_some(boost::asio::buffer(data, max_length),
+				error);
+			if (error == boost::asio::error::eof) {
+				std::cout << "connection closed by peer" << std::endl;
+				break;
+			}
+			else if (error) {
+				throw boost::system::system_error(error);
+			}
+
+			std::cout << "receive from "
+				<< sock->remote_endpoint().address().to_string() << std::endl;
+			std::cout << "receive message is " << data << std::endl;
+			boost::asio::write(*sock, boost::asio::buffer(data, length));
+		}
+		
+	}
+	catch (std::exception& e) {
+		std::cerr << "Exception in thread: " << e.what() << "\n" << std::endl;
+	}
+}
+
+void server(boost::asio::io_context& io_context, unsigned short port)
+{
+	tcp::acceptor a(io_context, tcp::endpoint(tcp::v4(), port));
+	for (;;) {
+		socket_ptr socket(new tcp::socket(io_context));
+		a.accept(*socket);
+		auto t = std::make_shared<std::thread>(session, socket);
+		thread_set.insert(t);
+	}
+}
+
+int main()
+{
+	try {
+		boost::asio::io_context ioc;
+		server(ioc, 10086);
+		for (auto& t : thread_set) {
+			t->join();
+		}
+	}
+	catch (std::exception& e) {
+		std::cerr << "Exception " << e.what() << "\n";
+	}
+}
